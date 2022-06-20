b287e5564c10f0e6d8be537ff43de8e5149f93c0
Akram <akram@itcplatform.de>
2022-06-20 16:29:07 +0200

    Updates

---
+++ app/js/main.js
@@ -0,0 +1,34 @@
+const container = "#contenedor";
+const titleContainer = "#title";
+
+$(() => {
+  FetchData();
+});
+
+///////////////////////// FETCH DATA FROM ZOHO /////////////////////////
+function FetchData() {
+  ZOHO.CREATOR.init().then(function () {
+    config = {
+      appName: "ITCP-Backoffice-v2",
+      reportName: "Release_Report",
+      criteria: "",
+    };
+
+    // Get data from Zoho
+    ZOHO.CREATOR.API.getAllRecords(config).then(function (response) {
+      var data = response.data;
+
+      var md = new remarkable.Remarkable();
+
+      for (var i = 0; i < data.length; i++) {
+        let auxRow = "<div class='row'>";
+        auxRow += "<div class='col d-flex flex-row align-items-center' id='subtitle'>" + md.render(data[i].Version) + "</div>";
+        auxRow += "</div>";
+        auxRow += "<div class='row mb-3'>"
+        auxRow += "<div class='col'>" + md.render(data[i].Text) + "</div>";
+        auxRow += "</div>";
+        $(container).append(auxRow);
+      }
+    });
+  });
+}
